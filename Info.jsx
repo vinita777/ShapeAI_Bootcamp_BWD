b0d8fc446504ba9857fd0273fa6c671e2f0b5bdc
vinita777 <85445838+vinita777@users.noreply.github.com>
2021-06-27 00:48:50 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+    return (
+      <div class="note">
+       <h1> Javascript and React.js </h1>
+       <p> A basic web dev React Js Bootcamps</p>
+      </div>
+    );
+}
+
+export default Info;
